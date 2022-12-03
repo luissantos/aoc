c46abff47f8f40a691f95e078bec93fe52c8a424
Luis Santos <luis@luissantos.pt>
2022-12-03 10:27:36 +0100

    AoC 2022 day 3

--- src/luissantos/aoc_2022.clj
+++ src/luissantos/aoc_2022.clj
@@ -37,7 +37,6 @@
     [:z :rock]  [:paper :rock]
     [:z :scissors] [:rock :scissors]))
 
-
 (defn day2-part2 []
   (let [hands {\A :rock \B :paper \C :scissors \X :x \Y :y \Z :z}]
     (->> (string/split-lines (slurp "input/day-2-input-1.txt"))
@@ -48,6 +47,38 @@
          (map round-score)
          (reduce +))))
 
+(defn char-range [start end]
+  (map char (range (int start) (inc (int end)))))
+
+(def priorities
+  (zipmap
+   (concat (char-range \a \z) (char-range \A \Z))
+   (range 1 53)))
+
+(defn parse-rucksack [rs]
+  (let [rs (seq rs)]
+    (split-at (/ (count rs) 2) rs)))
+
+(defn item-priority [col]
+  (->> col
+       (map set)
+       (apply clojure.set/intersection)
+       (first)
+       (priorities)))
+
+(defn day3-part1 []
+  (->> (string/split-lines (slurp "input/day-3-input-1.txt"))
+       (map parse-rucksack)
+       (map item-priority)
+       (reduce +)))
+
+(defn day3-part2 []
+  (->> (string/split-lines (slurp "input/day-3-input-1.txt"))
+       (partition 3)
+       (map seq)
+       (map item-priority)
+       (reduce +)))
+
 (defn -main
   [& args]
-  (println (day2-part2)))
+  (println (day3-part2)))
